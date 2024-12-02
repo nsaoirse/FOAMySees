d248fb8af5c3c82ab8b87a64efb82b178e88ad1b
nsaoirse <nicolette.s.lewis@outlook.com>
2024-12-02 14:49:00 -0800

    added additional example with OpenFOAM mesh which can handle deformation (previous verion was crashing due to nonorthogonality of CFD mesh, large erroneous forces)

---
+++ FOAMySees-Examples/CantileverBeam/caseSetup.sh
@@ -0,0 +1,15 @@
+export FYS_inputFilesLocation="fromUser"
+export FYS_OpenSeesPyModelFile="OpenSeesModel.py"
+export FYS_Nprocessors=1
+export FYS_OpenFOAMSolver="interFoam"
+export FYS_isPartOfHydro="No"
+export FYS_nameOfCoupledPatchOrSurfaceFile="interface"
+export FYS_useExistingOpenFOAMCaseFolder=1
+export FYS_ExistingOpenFOAMCase="Fluid"
+export FYS_HydrojsonFile="none"
+export FYS_numOpenSeesSubSteps=1
+export FYS_numOpenFOAMSubSteps=1
+export FYS_singlePhase="0"
+export FYS_singlePhaseDensity="1.225"
+export FYS_singlePhaseKinematicViscosity="1.48e-05"
+
